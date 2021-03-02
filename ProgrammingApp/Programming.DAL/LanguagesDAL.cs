21f7c59d11312827e16c7f01b020ec9dfd3bffcc
Okankaan <okankaan.cetinkaya@gmail.com>
2021-03-03 02:37:05 +0300

    First Web Api Example -Web Api CRUD Functions

---
+++ ProgrammingApp/Programming.DAL/LanguagesDAL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.DAL
+{
+    public class LanguagesDAL
+    {
+        ProgrammingDbEntities db = new ProgrammingDbEntities();
+
+        public IEnumerable<Languages> GetAllLanguages()
+        {
+            return db.Languages;
+        }
+
+        public Languages GetAllLanguageById(int id)
+        {
+            return db.Languages.Find(id);
+        }
+
+        public Languages CreateLanguage(Languages language)
+        {
+            db.Languages.Add(language);
+            db.SaveChanges();
+            return language;
+        }
+
+        public Languages UpdateLanguage(int id, Languages language)
+        {
+            db.Entry(language).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return language;
+        }
+
+        public void DeleteLanguage(int id)
+        {
+            db.Languages.Remove(db.Languages.Find(id));
+            db.SaveChanges();
+        }
+
+        public bool IsThereAnyLanguage(int id)
+        {
+            return db.Languages.Any(x => x.ID == id); //if there is any record in DB for coming id param ,return true.
+        }
+    }
+}

f400dd5d22c04383f038e58e4e6458da00098965
Okankaan <okankaan.cetinkaya@gmail.com>
2021-05-19 01:55:53 +0300

    -HelloService service reference added. -Form components designed. -Input Name sent to WCF Service(HelloService), name input processed by WCF Service, output data came from WCF Service.

--- SelfHostingWCFService/WindowsClient_WindowsFormProject/WindowsClient_WindowsFormProject/Form1.Designer.cs
+++ SelfHostingWCFService/WindowsClient_WindowsFormProject/WindowsClient_WindowsFormProject/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGetMessage = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGetMessage
+            // 
+            this.btnGetMessage.Location = new System.Drawing.Point(95, 145);
+            this.btnGetMessage.Name = "btnGetMessage";
+            this.btnGetMessage.Size = new System.Drawing.Size(175, 23);
+            this.btnGetMessage.TabIndex = 0;
+            this.btnGetMessage.Text = "Get Message";
+            this.btnGetMessage.UseVisualStyleBackColor = true;
+            this.btnGetMessage.Click += new System.EventHandler(this.btnGetMessage_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(92, 62);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(41, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name :";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMessage.Location = new System.Drawing.Point(92, 103);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 17);
+            this.lblMessage.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(170, 59);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(418, 315);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnGetMessage);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGetMessage;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtName;
     }
 }
 
--- SelfHostingWCFService/WindowsClient_WindowsFormProject/WindowsClient_WindowsFormProject/Form1.cs
+++ SelfHostingWCFService/WindowsClient_WindowsFormProject/WindowsClient_WindowsFormProject/Form1.cs
@@ -12,9 +12,27 @@ namespace WindowsClient_WindowsFormProject
 {
     public partial class Form1 : Form
     {
+        HelloService.HelloServiceClient client;
         public Form1()
         {
             InitializeComponent();
+            client = new HelloService.HelloServiceClient();
+        }
+
+        private void btnGetMessage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (client.State == System.ServiceModel.CommunicationState.Faulted)
+                {
+                    client = new HelloService.HelloServiceClient();
+                }
+                lblMessage.Text = client.GetMessage(txtName.Text);
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+            }
         }
     }
 }

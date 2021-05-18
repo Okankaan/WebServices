5159bcdd5f2488b6731bad8f5ea80777799d7325
Okankaan <okankaan.cetinkaya@gmail.com>
2021-05-19 02:44:05 +0300

    WebClient Project created for HelloService

---
+++ SelfHostingWCFService/WebClient_MVCProject/WebClient_MVCProject/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebClient_MVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string GetMessage(string name)
+        {
+            using (HelloService.HelloServiceClient client = new HelloService.HelloServiceClient())
+            {
+                string message = client.GetMessage(name);
+                return message;
+            }
+        }
+    }
+}
